7510fee149e8b0e9832f8589097dadce15ae5d17
Job Snijders <job@instituut.net>
2017-11-28 22:43:19 +0000

    Add coveralls

--- aggregate6/aggregate6.py
+++ aggregate6/aggregate6.py
@@ -37,13 +37,32 @@ import sys
 
 
 def aggregate(l):
+    """Aggregate a `list` of prefixes.
+
+    Keyword arguments:
+    l -- a python list of prefixes
+
+    Example use:
+    >>> aggregate(["10.0.0.0/8", "10.0.0.0/24"])
+    ['10.0.0.0/8']
+    """
     tree = radix.Radix()
     for item in l:
-        tree.add(item)
+        try:
+            tree.add(item)
+        except (ValueError) as err:
+            print("ERROR, invalid IP prefix: {}".format(item))
+            raise
+
     return aggregate_tree(tree).prefixes()
 
 
 def aggregate_tree(l_tree):
+    """Walk a py-radix tree and aggregate it.
+
+    Arguments
+    l_tree -- radix.Radix() object
+    """
 
     def _aggregate_phase1(tree):
         # check if prefix is already covered
