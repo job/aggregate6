9563b9e136a1366c9b7551353029922a3e495f71
Job Snijders <job@instituut.net>
2017-11-29 23:48:04 +0000

    Update manifest

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ if sys.argv[-1] == 'publish':
     print("You probably want to also tag the version now:")
     print("  git tag -a %s -m 'version %s'" % (version, version))
     print("  git push --tags")
+    print("  git push")
     sys.exit()
 
 
